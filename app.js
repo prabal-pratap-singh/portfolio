f3eca83df9bfdc30a63a029ade7f10464cac9eb1
prabal <ilovepokemonhoho@gmail.com>
2023-05-01 11:00:49 +0530

    added contact me page

--- app.js
+++ app.js
@@ -254,7 +254,18 @@ cursor.init();
 
 // customize cursor ---------------------------------------------------------------------
 
-
+function loadEvents() {
+  var mailString;
+  function updateMailString() {
+      mailString = '?subject=' + encodeURIComponent($('#subject').val())
+          + '&body=' + encodeURIComponent($('#message').val());
+      $('#mail-link').attr('href',  'mailto:pratapsinghprabal516@gmail.com' + mailString);
+  }
+  $( "#subject" ).focusout(function() { updateMailString(); });
+  $( "#message" ).focusout(function() { updateMailString(); });
+  updateMailString();
+  console.log(mailString)
+}
 
 
 
